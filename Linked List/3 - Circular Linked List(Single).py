1d7793472863f7f67774d74c93ae02b89e71c278
Rohan Singh Tomar <rohanstomar11@gmail.com>
2021-09-20 14:50:56 +0530

    minor changes

---
+++ Linked List/3 - Circular Linked List(Single).py
@@ -0,0 +1,13 @@
+'''
+Hello, This is purposely left empty for your practice.
+Implement a circular linked list with following methods:
+    - Displaying the list
+    - Adding data to the back
+    - Adding data in front
+    - Adding data at an index
+    - Deleting from front
+    - Deleting from back
+    - Deleting by index
+    - Deleting by value
+Let me know if you need any hint or help.
+'''
